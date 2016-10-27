33952a0bb210e8a99297aa84ce5f5be81ecb4c48
Tushar Maroo <tusharmaroo@gmail.com>
2016-10-27 11:17:23 +0530

    Adding gitlab clone it all
    Adding gitlab clone it all

--- clone_it_all.sh
+++ clone_it_all.sh
@@ -1,3 +1,5 @@
 curl -u tusharmaroo -s https://api.github.com/orgs/oragnization_name/repos\?per_page\=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
 
 curl -u tusharmaroo -s https://api.bitbucket.org/1.0/users/username | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read)["repositories"].each { |repo| %x[git clone git@bitbucket.org:username/#{repo["slug"]}.git] }'
+
+curl --header "PRIVATE-TOKEN: private_token" https://gitlab.domain.co.in/api/v3/groups/:id | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read)[:projects].each { |repo| %x[git clone #{repo[:ssh_url_to_repo]} }'
